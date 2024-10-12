c1fbda03db89935177987bb13c26960e8da0597c
Herrerasaurus <146883762+Herrerasaurus@users.noreply.github.com>
2024-10-12 13:59:24 -0700

    adding upgrade button - Step 5

--- src/main.ts
+++ src/main.ts
@@ -13,21 +13,26 @@ app.append(header);
 const button = document.createElement("button");
 button.innerHTML = "🦆";
 
+// Step 5: add new button
+const upgrade = document.createElement("button");
+upgrade.innerHTML = "More Ducks";
+
 // Step 2: Adding a counter
 let numDucks: number = 0;
 const displayDucks = document.createElement("counter");
-displayDucks.innerHTML = `<br><br>${numDucks} Ducks`;
+displayDucks.innerHTML = `<br><br>${numDucks} Ducks<br><br>`;
+
+let addCount = 0;
 
 button.onclick = () => {
-    updateDucks();
+    updateDucks(1);
 };
 
 // Step 3: Automatic Clicking
 // removing for step 4 // const interval = setInterval(updateDucks, 1000);
-
-function updateDucks(){
-    numDucks++;
-    displayDucks.innerHTML = `<br><br>${numDucks} Ducks`;
+function updateDucks(x){
+    numDucks+= x;
+    displayDucks.innerHTML = `<br><br>${numDucks} Ducks<br><br>`;
 }
 
 // Step 4: Continuous Growth
@@ -37,14 +42,23 @@ requestAnimationFrame(contGrowth);
 function contGrowth(time){
     if((time - timestamp >= 1000) ){
         timestamp = time;
-        updateDucks();
+        updateDucks(addCount);
     }
+    
     requestAnimationFrame(contGrowth);
 }
+
+// Step 5: Purchasing an upgrade
+upgrade.onclick = () => {
+    addCount += 1;
+    numDucks -= 10;
+};
+
 
 
 app.append(button);
 app.append(displayDucks);
+app.append(upgrade);
 
 
 
